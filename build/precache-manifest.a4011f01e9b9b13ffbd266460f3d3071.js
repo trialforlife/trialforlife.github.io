c79d70a896dee56dc19be1e32b981badcfb76753
Andrew Kravchuk <>
2018-10-20 15:28:27 +0300

    hackaton

---
+++ build/precache-manifest.a4011f01e9b9b13ffbd266460f3d3071.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a6ceac620690dbe66c1d",
+    "url": "/static/js/main.a6ceac62.chunk.js"
+  },
+  {
+    "revision": "c2fe7a64faa17c95737c",
+    "url": "/static/js/1.c2fe7a64.chunk.js"
+  },
+  {
+    "revision": "a6ceac620690dbe66c1d",
+    "url": "/static/css/main.b92a2c3a.chunk.css"
+  },
+  {
+    "revision": "796d074a2bac02935a5d912cc0c7e6c0",
+    "url": "/index.html"
+  }
+];
