d28f264dcd7b156fc75696e9a6ba4d1b165cd56c
Andrew Kravchuk <>
2018-10-21 10:24:52 +0300

    fix router

---
+++ precache-manifest.db8794066b744bda6dd41aa9090c7c23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b299b001441984388fee",
+    "url": "/static/js/main.b299b001.chunk.js"
+  },
+  {
+    "revision": "3980befa628e804b3541",
+    "url": "/static/js/1.3980befa.chunk.js"
+  },
+  {
+    "revision": "b299b001441984388fee",
+    "url": "/static/css/main.16826e9f.chunk.css"
+  },
+  {
+    "revision": "73485d2c6f1c633c4d7e4597b1781dd7",
+    "url": "/index.html"
+  }
+];
